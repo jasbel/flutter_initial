84562fd14b3a6d94d538fca2076a7a32af9f3d2e
jasbel <asbel.elmec@gmail.com>
2021-03-29 22:28:22 -0400

    terminated code flutter basic

--- lib/main.dart
+++ lib/main.dart
@@ -31,23 +31,75 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  // int _counter = 0;
+
+  // void _incrementCounter() {
+  //   setState(() {
+
+  //   });
+  // }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Textos'),
-      ),
-      body: Column(
-        children: <Widget>[
-          Text(
-            'texto prueba estandar',
-            style: TextStyle(fontWeight: FontWeight.bold),
-          )
-        ],
+          // title: Text('Textos'),
+          ),
+      body: Container(
+        // color: Colors.yellow,
+        margin: EdgeInsets.only(top: 24, left: 16),
+        decoration: BoxDecoration(
+            border: Border.all(color: Colors.black, width: 5),
+            shape: BoxShape.circle,
+            color: Colors.blue),
+        padding: EdgeInsets.all(18),
+        child: Icon(
+          Icons.accessible_forward,
+          color: Colors.white,
+        ),
       ),
     );
   }
 }
+
+// class _MyHomePageState extends State<MyHomePage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Textos'),
+//       ),
+//       body: Column(
+//         children: <Widget>[
+//           SizedBox(
+//             width: MediaQuery.of(context).size.width,
+//             child: Text(
+//               'texto prueba estandar',
+//               textAlign: TextAlign.center,
+//               style: TextStyle(
+//                   fontWeight: FontWeight.bold,
+//                   fontSize: 16,
+//                   color: Colors.grey),
+//             ),
+//           ),
+//           RichText(
+//               text: TextSpan(
+//                   text: 'Texto enriquecido',
+//                   style: TextStyle(fontSize: 16, color: Colors.black),
+//                   children: <TextSpan>[
+//                 TextSpan(
+//                   text: 'bold',
+//                   style: TextStyle(
+//                       fontWeight: FontWeight.bold, color: Colors.blue),
+//                 ),
+//                 TextSpan(text: 'Texto Regular'),
+//               ]))
+//         ],
+//       ),
+//     );
+//   }
+// }
+
 // class _MyHomePageState extends State<MyHomePage> {
 //   @override
 //   Widget build(BuildContext context) {
